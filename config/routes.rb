754ea91ee9a7ea1a7eee1020db8a8b7386a85ea0
田中 真奈 <tanakamana@tanaka-no-MacBook-Air.local>
2016-09-18 14:07:17 +0900

    Add resources :prototypes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, :only => [:show]
   root 'prototypes#index'
 
-  get  '/prototypes/index'  =>    'prototypes#index'
-  get  '/prototypes/new'    =>    'prototypes#new'
-  get  '/prototypes/show'   =>    'prototypes#show'
+  resources :users, :only => [:show]
+  resources :prototypes, :only [:index, :new, :show]
 end
