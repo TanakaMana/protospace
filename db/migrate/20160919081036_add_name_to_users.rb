066a61de86502767149aecb6dd0bc6a606288643
田中 真奈 <tanakamana@tanaka-no-MacBook-Air.local>
2016-09-21 00:51:17 +0900

    Rewrite Users column.

--- db/migrate/20160919081036_add_name_to_users.rb
+++ db/migrate/20160919081036_add_name_to_users.rb
@@ -1,5 +1,9 @@
 class AddNameToUsers < ActiveRecord::Migration
   def change
     add_column :users, :name, :string
+    add_column :users, :image, :text
+    add_column :users, :member, :string
+    add_column :users, :profile, :text
+    add_column :users, :work, :string
   end
 end
--- db/migrate/20160919081631_add_work_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddWorkToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :work, :string
-  end
-end
