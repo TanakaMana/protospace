bad4042265de3c7aac61f71293c47cb818ab5beb
田中 真奈 <tanakamana@tanaka-no-MacBook-Air.local>
2016-10-01 17:56:26 +0900

    fix database of prototypes

--- db/migrate/20160925023859_add_user_id_to_prototypes.rb
+++ db/migrate/20160925023859_add_user_id_to_prototypes.rb
@@ -1,5 +1,11 @@
 class AddUserIdToPrototypes < ActiveRecord::Migration
   def change
     add_reference :prototypes, :user, index: true, foreign_key: true
+    add_column :prototypes, :title, :string
+    add_column :prototypes, :catchcopy, :string
+    add_column :prototypes, :concept, :text
+    remove_column :prototypes, :prototype_id_id, :references
+    remove_column :prototypes, :image, :text
+    remove_column :prototypes, :state, :integer
   end
 end
