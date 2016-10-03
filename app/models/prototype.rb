5cdf9f1c01e0d8eec8da6257d321859a6fd8d6f2
田中 真奈 <tanakamana@tanaka-no-MacBook-Air.local>
2016-10-03 19:08:16 +0900

    add uploader and association and nest

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,3 +1,6 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
+  has_many :prototype_thumbnails
+  accepts_nested_attributes_for :prototype_thumbnails, reject_if: proc { |attributes| attributes['image'].blank? }
+  validates_presence_of :title, :catchcopy, :concept
 end
