46018306097cb9771f41d055017196bd2767db65
田中 真奈 <tanakamana@tanaka-no-MacBook-Air.local>
2016-10-05 14:53:26 +0900

    method of exception handling

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :prototype_thumbnails
-  accepts_nested_attributes_for :prototype_thumbnails, reject_if: proc { |attributes| attributes['image'].blank? }
+  accepts_nested_attributes_for :prototype_thumbnails, reject_if: :reject_sub_images
   validates_presence_of :title, :catchcopy, :concept
 end
