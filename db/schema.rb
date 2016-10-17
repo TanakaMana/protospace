67837646976bc36c840239fae0448f14bf69666e
田中 真奈 <tanakamana@tanaka-no-MacBook-Air.local>
2016-10-17 22:48:36 +0900

    create table of likes and comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161003004802) do
+ActiveRecord::Schema.define(version: 20161017134634) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id",      limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.text     "content",      limit: 65535
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",      limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "prototype_thumbnails", force: :cascade do |t|
     t.integer  "prototype_id", limit: 4
