b00252d42eb1d06fcc305f589a9c834cc727eda1
田中 真奈 <tanakamana@tanaka-no-MacBook-Air.local>
2016-10-19 13:27:51 +0900

    set association of user, prototype, comment, like

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,9 +2,16 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :prototype_thumbnails
   accepts_nested_attributes_for :prototype_thumbnails, reject_if: :reject_sub_images
-  validates_presence_of :title, :catchcopy, :concept
-end
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
-def reject_sub_images(attributes)
+  validates_presence_of :title, :catchcopy, :concept
+
+  def reject_sub_images(attributes)
   attributes['image'].blank?
+  end
+
+  def like_user(user_id)
+    likes.find_by(user_id: user_id)
+  end
 end
