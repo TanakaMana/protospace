2f1a47e89caa3fd14e06aacfe20440ca727d082e
田中 真奈 <tanakamana@tanaka-no-MacBook-Air.local>
2016-10-20 23:56:12 +0900

    add nest routing of popular and newest prototypes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'prototypes#index'
+  root 'prototypes/popular#index'
 
+  resources :users, only: [:show, :edit, :update]
   resources :prototypes do
     resources :likes, only: [:create, :destroy]
     resources :comments, only: [:create]
   end
-  resources :users, only: [:show, :edit, :update]
+  scope module: :prototypes do
+    resources :popular, only: :index
+    resources :newest, only: :index
+  end
 end
