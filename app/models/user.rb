d93bf920d8859d0e862cc394ac99bc1e51c0ec4f
田中 真奈 <makkuromyaon@gmail.com>
2016-10-28 04:18:39 +0900

    add uploader of user_image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  mount_uploader :image, UserImageUploader
   has_many :prototypes
   has_many :likes
   has_many :comments
