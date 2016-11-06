7733071990aaf6622ba1d657b4f8124103726b5e
田中 真奈 <makkuromyaon@gmail.com>
2016-11-06 17:34:08 +0900

    add return value for returns error

--- spec/models/prototype_thumbnail_spec.rb
+++ spec/models/prototype_thumbnail_spec.rb
@@ -4,7 +4,7 @@ describe PrototypeThumbnail do
   describe '#create' do
     describe "with valid attributes" do
       it "has a valid factory" do
-        prototype_thumbnail = build(:prototype_thumbnail)
+        prototype_thumbnail = build(:prototype_thumbnail, :main)
         expect(prototype_thumbnail).to be_valid
       end
     end
@@ -13,7 +13,7 @@ describe PrototypeThumbnail do
       it "returns error" do
         prototype_thumbnail = build(:prototype_thumbnail, image: nil)
         prototype_thumbnail.valid?
-        expect(prototype_thumbnail.errors[:image])
+        expect(prototype_thumbnail.errors[:image]).to include("can't be blank")
       end
     end
 
@@ -21,7 +21,7 @@ describe PrototypeThumbnail do
       it "returns error" do
         prototype_thumbnail = build(:prototype_thumbnail, state: nil)
         prototype_thumbnail.valid?
-        expect(prototype_thumbnail.errors[:state])
+        expect(prototype_thumbnail.errors[:state]).to include("can't be blank")
       end
     end
   end
