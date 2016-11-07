56fbf5208306830ac4af86b224073bbd277b863a
田中 真奈 <makkuromyaon@gmail.com>
2016-11-07 13:48:45 +0900

    add devise controller at rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,12 +5,16 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/poltergeist'
+require 'devise'
+require 'support/controller_macros'
 Capybara.javascript_driver = :poltergeist
 
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.extend ControllerMacros, type: :controller
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
