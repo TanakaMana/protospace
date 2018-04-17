96d4df2bfe0845bd10f046dd6c8d445fa399b263
TanakaMana <makkuromyaon@gmail.com>
2018-04-17 16:06:07 +0900

    capistrano nokogiriエラーへの対処を追加

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,4 +3,8 @@ set :branch, 'staging'
 set :deploy_to, '/var/www/staging'
 set :rails_env, 'staging'
 set :bundle_env_variables, { 'NOKOGIRI_USE_SYSTEM_LIBRARIES' => 1 }
+set :default_env, {
+#    'PKG_CONFIG_PATH' => '/opt/local/bin'
+  'PATH' => "/opt/local/bin:$PATH"
+}
 server '52.197.135.183', user: 'ec2-user', roles: %w{app db web}
