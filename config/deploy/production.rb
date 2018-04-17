c9f21c02429caa5a3d361b0dbb6e9f68b3bf81c0
TanakaMana <makkuromyaon@gmail.com>
2018-04-17 19:11:23 +0900

    設定の見直し

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :application, 'protospace'
 set :branch, 'master'
 set :deploy_to, '/var/www/protospace'
 set :rails_env, 'production'
-server '52.197.135.183', user: 'ec2-user', roles: %w{app db web}
+server 'www.tnmn.work', user: 'ec2-user', roles: %w{app db web}
