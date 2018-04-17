256a9f0341db22577240e104b1b9574e3ca158f8
TanakaMana <makkuromyaon@gmail.com>
2018-04-17 17:56:18 +0900

    修正

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,9 +1,5 @@
 server '52.197.135.183', user: 'ec2-user', roles: %w{app db web}
 
-role :app, %w{ec2-user@52.197.135.183}
-role :web, %w{ec2-user@52.197.135.183}
-role :db,  %w{ec2-user@52.197.135.183}
-
 set :application, 'staging'
 set :branch, 'staging'
 set :deploy_to, '/var/www/staging'
