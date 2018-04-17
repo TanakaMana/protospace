8760b047d99ff263aed4f398a952a10f8f798493
TanakaMana <makkuromyaon@gmail.com>
2018-04-17 17:52:37 +0900

    deploy設定の調整

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,11 @@
+server '52.197.135.183', user: 'ec2-user', roles: %w{app db web}
+
+role :app, %w{ec2-user@52.197.135.183}
+role :web, %w{ec2-user@52.197.135.183}
+role :db,  %w{ec2-user@52.197.135.183}
+
 set :application, 'protospace'
 set :branch, 'master'
 set :deploy_to, '/var/www/protospace'
 set :rails_env, 'production'
-server '52.197.135.183', user: 'ec2-user', roles: %w{app db web}
+set :stage, :production
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,10 +1,11 @@
+server '52.197.135.183', user: 'ec2-user', roles: %w{app db web}
+
+role :app, %w{ec2-user@52.197.135.183}
+role :web, %w{ec2-user@52.197.135.183}
+role :db,  %w{ec2-user@52.197.135.183}
+
 set :application, 'staging'
 set :branch, 'staging'
 set :deploy_to, '/var/www/staging'
 set :rails_env, 'staging'
-set :bundle_env_variables, { 'NOKOGIRI_USE_SYSTEM_LIBRARIES' => 1 }
-set :default_env, {
-#    'PKG_CONFIG_PATH' => '/opt/local/bin'
-  'PATH' => "/opt/local/bin:$PATH"
-}
-server '52.197.135.183', user: 'ec2-user', roles: %w{app db web}
+set :stage, :staging
