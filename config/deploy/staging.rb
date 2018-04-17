1898ffc9108bd9f778326fa2b08c1d18a43414b8
TanakaMana <makkuromyaon@gmail.com>
2018-04-17 15:28:53 +0900

    stagingのbundle installの設定を追加

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,4 +2,5 @@ set :application, 'staging'
 set :branch, 'staging'
 set :deploy_to, '/var/www/staging'
 set :rails_env, 'staging'
+set :bundle_env_variables, { 'NOKOGIRI_USE_SYSTEM_LIBRARIES' => 1 }
 server '52.197.135.183', user: 'ec2-user', roles: %w{app db web}
