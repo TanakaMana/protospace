ca2d8d62eac30ee2bb910c0882f13e0f93209f39
TanakaMana <makkuromyaon@gmail.com>
2018-04-17 18:45:56 +0900

    Revert "serverの名前を変更"
    This reverts commit c6e8dc0a191ad6dae1c5a7987bb776fdfc2f6e9e.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,4 +7,4 @@ set :default_env, {
 #    'PKG_CONFIG_PATH' => '/opt/local/bin'
   'PATH' => "/opt/local/bin:$PATH"
 }
-server 'staging.tnmn.work', user: 'ec2-user', roles: %w{app db web}
+server '52.197.135.183', user: 'ec2-user', roles: %w{app db web}
