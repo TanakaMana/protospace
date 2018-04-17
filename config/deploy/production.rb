58606ba85b469dad60a0bad6aeef260bf461a083
TanakaMana <makkuromyaon@gmail.com>
2018-04-17 17:57:45 +0900

    Revert "deploy設定の調整"
    This reverts commit 8760b047d99ff263aed4f398a952a10f8f798493.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,5 @@
-server '52.197.135.183', user: 'ec2-user', roles: %w{app db web}
-
-role :app, %w{ec2-user@52.197.135.183}
-role :web, %w{ec2-user@52.197.135.183}
-role :db,  %w{ec2-user@52.197.135.183}
-
 set :application, 'protospace'
 set :branch, 'master'
 set :deploy_to, '/var/www/protospace'
 set :rails_env, 'production'
-set :stage, :production
+server '52.197.135.183', user: 'ec2-user', roles: %w{app db web}
